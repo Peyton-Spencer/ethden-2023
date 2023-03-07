57ca18758177fc049c85598a52b330bd8338105d
pspencer-arculus <pspencer@arculus.co>
2023-03-07 13:42:29 -0700

    add some erc1155 tests

--- src/DayPlace.sol
+++ src/DayPlace.sol
@@ -4,31 +4,30 @@ pragma solidity >=0.8.19;
 import "@openzeppelin/contracts/access/Ownable.sol";
 import "@openzeppelin/contracts/token/ERC1155/ERC1155.sol";
 
+/// @notice This is the time period for which the rate is charged.
+enum RatePeriod {
+    DAILY,
+    HOURLY
+}
+
+struct PropertyMetadata {
+    RatePeriod period;
+    uint256 ratePerPeriod;
+    uint256 securityDeposit;
+    /// @notice map of unix timestamps to availability
+    /// @dev days start at midnight UTC (12:00 AM)
+    bytes calendar;
+}
+
 /// @notice The Day Marketplace contract.
 contract DayPlace is ERC1155 {
     // mapping (uint256 => uint256) public
     // OwnerHub
 
-    /// @notice This is the time period for which the rate is charged.
-    enum RatePeriod {
-        DAILY,
-        HOURLY
-    }
-
     uint256 public constant DAYS_TOKEN_FT = 0 << 128;
     uint256 public constant PROPERTY_TOKEN_NFT = 1 << 128;
     uint256 public constant STAY_TOKEN_NFT = 2 << 128;
     uint128 public propertyTokenNftIndex = 1;
-
-    struct PropertyMetadata {
-        address owner;
-        RatePeriod period;
-        uint256 ratePerPeriod;
-        uint256 securityDeposit;
-        /// @notice map of unix timestamps to availability
-        /// @dev days start at midnight UTC (12:00 AM)
-        bytes calendar;
-    }
 
     mapping(uint256 => PropertyMetadata) public metadataByPropertyID;
     Stay[][] public staysByPropertyID;
@@ -42,10 +41,23 @@ contract DayPlace is ERC1155 {
         uint256 endTime;
     }
 
-    function mintProperty() external {
+    function mintProperty(PropertyMetadata memory meta) external {
         bytes memory data;
-        _mint(msg.sender, PROPERTY_TOKEN_NFT + propertyTokenNftIndex, 1, data);
+        uint256 id = nextPropertyID();
+        _mint(_msgSender(), id, 1, data);
+        metadataByPropertyID[id] = meta;
         propertyTokenNftIndex++;
+    }
+
+    function batchMintProperty(
+        PropertyMetadata[] memory meta,
+        uint256[] memory ids,
+        uint256[] memory amounts
+    )
+        external
+    {
+        bytes memory data;
+        _mintBatch(_msgSender(), ids, amounts, data);
     }
 
     function mintDaysForProperty(uint256 propertyID, uint256 dayCount) external { }
@@ -53,4 +65,8 @@ contract DayPlace is ERC1155 {
     // function _ownerOf(uint256 propertyID) {
     //     balanceOf(msg.sender, propertyID);
     // }
+
+    function nextPropertyID() public view returns (uint256) {
+        return PROPERTY_TOKEN_NFT + propertyTokenNftIndex;
+    }
 }
--- test/DayPlace.t.sol
+++ test/DayPlace.t.sol
@@ -4,10 +4,42 @@ pragma solidity >=0.8.19 <0.9.0;
 import { PRBTest } from "@prb/test/PRBTest.sol";
 import { console2 } from "forge-std/console2.sol";
 import { StdCheats } from "forge-std/StdCheats.sol";
-import "../src/DayPlace.sol";
+import "@openzeppelin/contracts/token/ERC1155/IERC1155Receiver.sol";
+import { DayPlace, PropertyMetadata, RatePeriod } from "../src/DayPlace.sol";
 
-contract PassFactoryTest is PRBTest, StdCheats {
-    DayPlace internal marketplace = new DayPlace(vm.envString("URI"));
+contract ERC1155ReceiverDoNothing {
+    function onERC1155Received(
+        address, /* operator */
+        address, /* from */
+        uint256, /* id */
+        uint256, /* value */
+        bytes calldata /* data */
+    )
+        external
+        pure
+        returns (bytes4)
+    {
+        return bytes4(keccak256("onERC1155Received(address,address,uint256,uint256,bytes)"));
+    }
+
+    function onERC1155BatchReceived(
+        address, /* operator */
+        address, /* from */
+        uint256[] calldata, /* ids */
+        uint256[] calldata, /* values */
+        bytes calldata /* data */
+    )
+        external
+        pure
+        returns (bytes4)
+    {
+        return bytes4(keccak256("onERC1155BatchReceived(address,address,uint256[],uint256[],bytes)"));
+    }
+}
+
+contract DayPlaceTest is PRBTest, StdCheats {
+    DayPlace private marketplace = new DayPlace(vm.envString("URI"));
+    ERC1155ReceiverDoNothing private receiver = new ERC1155ReceiverDoNothing();
 
     // function setUp() public { }
 
@@ -15,5 +47,56 @@ contract PassFactoryTest is PRBTest, StdCheats {
         string memory uri = vm.envString("URI");
         console2.log(uri);
         assertEq(marketplace.uri(1), uri);
+    }
+
+    function testSingleMintProperty(
+        uint256 _ratePerPeriod,
+        uint256 _securityDeposit,
+        bytes memory _calendar
+    )
+        external
+    {
+        _mintPropertyToReceiver(_ratePerPeriod, _securityDeposit, _calendar);
+        uint256 expectedID = marketplace.nextPropertyID() - 1;
+        assert(marketplace.balanceOf(address(receiver), expectedID) == 1);
+
+        (RatePeriod p, uint256 rate, uint256 deposit,) = marketplace.metadataByPropertyID(expectedID);
+        console2.log("rate", rate, "deposit", deposit);
+        console2.log("ratePeriod", uint256(p));
+    }
+
+    function testMultiMintProperty(
+        uint8 _mintCount,
+        uint256 _ratePerPeriod,
+        uint256 _securityDeposit,
+        bytes memory _calendar
+    )
+        external
+    {
+        for (uint256 index = 0; index < _mintCount; index++) {
+            _mintPropertyToReceiver(_ratePerPeriod, _securityDeposit, _calendar);
+            uint256 expectedID = marketplace.nextPropertyID() - 1;
+            assert(marketplace.balanceOf(address(receiver), expectedID) == 1);
+        }
+    }
+
+    function _mintPropertyToReceiver(
+        uint256 _ratePerPeriod,
+        uint256 _securityDeposit,
+        bytes memory _calendar
+    )
+        private
+    {
+        // set msg.sender to the receiver contract
+        address recv = address(receiver);
+        vm.prank(recv);
+        PropertyMetadata memory meta = PropertyMetadata({
+            period: RatePeriod.HOURLY,
+            ratePerPeriod: _ratePerPeriod,
+            securityDeposit: _securityDeposit,
+            calendar: _calendar
+        });
+
+        marketplace.mintProperty(meta);
     }
 }
